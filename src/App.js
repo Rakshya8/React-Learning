18139fed46d8ccc8ace60774ee27acc6c5e633f1
Rakshya Moktan <moktanrakshya1@gmail.com>
2022-06-29 15:08:41 +0545

    Completed tutorial:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Hello from './sayHello';
+import Tweet from './Tweet';
 
-function App() {
+function App(){
+  const [isRed, setRed] = useState(false);
+  const [count, setCount]=useState(0);
+  const [isOdd, setOdd]= useState(false);
+  const increment = () => {
+    setCount (count+1);
+    setRed(!isRed);
+    setOdd(!isOdd);
+  }
+  const [users, setUsers]=useState([
+    {name: 'Ed', message: 'Hello There' , likes: '100'},
+    {name: 'Ram', message: 'Party' , likes: '10'},
+    {name: 'Stephen Curry', message: 'GOAT' , likes: '200'},
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+       <div className="app">        
+        {/* <Hello /> */}
+        {/* <Tweet name="One" message="This is a random tweet" likes="50"/>
+        <Tweet name="Two" message="This is a random tweet two" likes="600"/>
+        <Tweet name="Three" message="This is a random tweet three" likes="300"/>
+        <Tweet name="Four" message="This is a random tweet four" likes="100"/> */}
+        {/* <h1 class={isRed ? "red" : "blue"}>{isOdd ? "Odd" : "Even"}</h1>
+        <button onClick ={increment}>Increment </button>
+        <h1>{count}</h1> */}
+        
+        {users.map(user =>(
+          <Tweet name={user.name} message={user.message} likes={user.likes}/>
+        ))}
+        </div>
   );
 }
-
 export default App;
---
+++ src/sayHello.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Hello(){
+    const sayHello=()=>{
+        console.log("hello");
+    
+      }
+    
+      return (
+           <div>
+             <h3>This Hello Component</h3>
+             <button onClick={sayHello}>Hello</button>
+            </div>
+      );
+    }
+    export default Hello;
